4410956bc046c70a8a9610b5075b5f1ce6b38eec
Reinaldo Bonet <bonetr@tcd.ie>
2021-03-29 23:42:58 +0100

    Gym and Coffee fix
    added a route to the the index.js file for /gym
    
    added svg files and connected them to /coffee and /gym
    
    Made them flexible and fill the page and stick to the menu using ‘img-fluid’
    Made them flexible and fill the page and stick to the menu using ‘img-fluid’

--- index.js
+++ index.js
@@ -11,20 +11,26 @@ app.set("view_engine", "ejs");
 app.set("views", "templates");
 
 app.listen(port);
-console.log("Server running on http://localhost:" + port);              //this will display xxxx
+console.log("Server running on http://localhost:" + port); //this will display xxxx
 
-app.get("/", function (request, response) {
+app.get("/", function(request, response) {
 
-    response.render("index.ejs",{
-        "events":events,
+    response.render("index.ejs", {
+        "events": events,
         "flag": flag
     });
 
 });
 
-app.get("/coffee", function (request, response) {
+app.get("/coffee", function(request, response) {
 
     response.render("coffee.ejs");
+
+});
+
+app.get("/gym", function(request, response) {
+
+    response.render("gym.ejs");
 
 });
 
@@ -32,41 +38,40 @@ app.get("/coffee", function (request, response) {
 
 
 var flag = false;
-const events=[
-    {
-        title:"Google Techtalk",
-        date:"May 2nd 2021 @9AM",
-        type:"Live event",
-        url:"https://talksat.withgoogle.com/"
+const events = [{
+        title: "Google Techtalk",
+        date: "May 2nd 2021 @9AM",
+        type: "Live event",
+        url: "https://talksat.withgoogle.com/"
     },
     {
-        title:"Networking Beer Party",
-        date:"May 2nd 2021 @11PM",
-        type:"Live event",
-        url:"https://www.eventbars.ie/party-home-bars-beer-kegs-hire-ireland/"
+        title: "Networking Beer Party",
+        date: "May 2nd 2021 @11PM",
+        type: "Live event",
+        url: "https://www.eventbars.ie/party-home-bars-beer-kegs-hire-ireland/"
     },
     {
-        title:"Accenture Recruitment Talk",
-        date:"May 4th 2021 @9AM",
-        type:"Live event",
-        url:"https://youtu.be/2q0da_2wonA"
+        title: "Accenture Recruitment Talk",
+        date: "May 4th 2021 @9AM",
+        type: "Live event",
+        url: "https://youtu.be/2q0da_2wonA"
     },
     {
-        title:"Accenture LinkedIn Workshop",
-        date:"May 4th 2021 @12PM",
-        type:"Live event",
-        url:"https://www.linkedin.com/company/accenture/?originalSubdomain=ie"
+        title: "Accenture LinkedIn Workshop",
+        date: "May 4th 2021 @12PM",
+        type: "Live event",
+        url: "https://www.linkedin.com/company/accenture/?originalSubdomain=ie"
     },
     {
-        title:"Microsoft UX Talk",
-        date:"May 8th 2021 @9AM",
-        type:"Live event",
-        url:"https://ux.microsoft.com/Talk/EdgeBrowserServices"
+        title: "Microsoft UX Talk",
+        date: "May 8th 2021 @9AM",
+        type: "Live event",
+        url: "https://ux.microsoft.com/Talk/EdgeBrowserServices"
     },
     {
-        title:"Free Gym day",
-        date:"May 9th 2021 @All Day",
-        type:"In-House Event",
-        url:"#"
+        title: "Free Gym day",
+        date: "May 9th 2021 @All Day",
+        type: "In-House Event",
+        url: "#"
     }
 ];
