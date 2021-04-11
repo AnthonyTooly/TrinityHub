def4d608fcc1a2b6f5e2c08931e157cd2d2526e3
Steph <Steph@DESKTOP-PLHCF22.local>
2021-04-11 13:26:24 +0100

    - Small CSS changes to profile and contact page

--- databse/group4_assignment3_events.sql
+++ databse/group4_assignment3_events.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: group4_assignment3
 -- ------------------------------------------------------
--- Server version	8.0.22
+-- Server version	8.0.23
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -53,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-08 21:23:00
+-- Dump completed on 2021-04-07 14:22:45
